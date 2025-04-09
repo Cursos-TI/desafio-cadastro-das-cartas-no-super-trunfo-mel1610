4bed9f26d9bc2dc3793f198eb173875448c831fd
mel1610 <melissanascimento02@gmail.com>
2025-04-09 23:54:30 +0000

    printf sao paulo

---
+++ nov.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+char estado ='A';
+char codigo [20] = "AO1";
+char nome [20] = "Sao paulo";
+int populacao = 12325000;
+float area = 1511;
+float PIB = 699;
+int pontos = 50;
+
+printf("estado: %c\n", estado);
+    printf("codigo: %s\n", codigo);
+    printf("nome: %s\n", nome);
+    printf("populacao: %d\n",populacao);
+    printf("area: %e km\n", area);
+    printf("PIB: %f bilhoes\n", PIB);
+    printf("pontos: %d pontos turisticos\n", pontos);
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+}
