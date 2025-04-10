6acf5fb5d06da35823acddae39f436ad62050738
mel1610 <melissanascimento02@gmail.com>
2025-04-10 00:23:01 +0000

    rio

---
+++ rio.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+    char estado = 'B';
+char codigo[20] = "BO2";
+char nome[20] = "Rio de Jnaeiro";
+int populacao = 6748000;
+float area = 1200.25;
+float PIB = 300.50;
+int pontos = 30;
+
+printf("estdado %c\n", estado);
+printf("codigo: %s\n",codigo);
+printf("nome: %s\n",nome);
+printf("populacao: %d\n", populacao);
+printf("area: %e km\n", area);
+printf("PIB: %f bilhoes\n", PIB);
+printf("pontos: %d pontos turisticos\n", pontos);
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
